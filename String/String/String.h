a21641a5c7e76c3e93bbf3162adec30f8bbabd76
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2018-10-02 15:17:21 +0200

    Finished string class

--- String/String/String.h
+++ String/String/String.h
@@ -13,14 +13,14 @@ public:
 
 	String(const char* string) : 
 		num_allocated(strlen(string)), 
-		str(new char[num_allocated])
+		str(new char[num_allocated + 1])
 	{
 		strcpy(str, string);
 	}
 
 	String(const String &string) : 
 		num_allocated(string.num_allocated),
-		str(new char[num_allocated])
+		str(new char[num_allocated + 1])
 	{
 		strcpy(str, string.str);
 	}
@@ -40,24 +40,40 @@ public:
 		str = nullptr;
 		num_allocated = 0;
 	}	
+
+	uint allocatedSpace() {
+		return num_allocated;
+	}
 	
 	//Operators
-	void operator=(String &string) {
-		str = string.str;
+	void operator+=(String &string) {
+		uint newAllocated = num_allocated + strlen(string.str);
+		char* newString = new char[newAllocated + 1];
+		strcpy(newString, str);
+		strcat(newString, string.str);
+		clear();
+		num_allocated = newAllocated;
+		str = newString;
 	}
 
-	void operator+=(String &string) {
-		
+	void operator=(String &string) {
+		num_allocated += strlen(string.str);
+		str = string.str;
 	}
 
 	bool operator==(String &string) const {
 		return (str == string.str);
 	}
 
+	String operator+(String &string) {
+		String newString(str);
+		newString += string;
+		return newString;
+	}
+
 private:
 	char* str = nullptr;
 	uint num_allocated = 0;
-
 };
 
 #endif // __STRING_H__
