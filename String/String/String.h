d80e06d040be27d4a96c366106f6cc9d1e4c446b
DLorenzoLaguno17 <36265574+DLorenzoLaguno17@users.noreply.github.com>
2018-10-02 14:36:26 +0200

    created constructors

--- String/String/String.h
+++ String/String/String.h
@@ -1,13 +1,44 @@
 #ifndef __STRING_H__
 #define __STRING_H__
 
+#include <cstring>
+
+typedef unsigned int uint;
+
 class String {
 
 public:
+	//Constructors
+	String() {}
 
-public:
+	String(const char* string) : 
+		num_allocated(strlen(string)), 
+		str(new char[num_allocated])
+	{
+		strcpy(str, string);
+	}
+
+	String(const String &string) : 
+		num_allocated(string.num_allocated),
+		str(new char[num_allocated])
+	{
+		strcpy(str, string.str);
+	}
+	
+	//Destructor
+	~String() {
+		clear();
+	}
+
+	//Methods
+	void clear() {
+
+	}
 
 private:
+	char* str = nullptr;
+	uint num_allocated = 0;
+
 };
 
 #endif // __STRING_H__
